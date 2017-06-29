88521043a908de133ad92a89486dc2e36e4362fc
Tumelo <tumelophuti@gmail.com>
2017-06-29 10:04:30 +0200

    First Commit

---
+++ XamarinFPieChart/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CrossPieCharts.FormsPlugin.Abstractions;
+using Xamarin.Forms;
+//using Xamarin.Plugin.CrossPieChart;
+
+namespace XamarinFPieChart
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CrossPieChartSample().GetPageWithPieChart();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinFPieChart/XamarinFPieChartPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFPieChart
+{
+    public partial class XamarinFPieChartPage : ContentPage
+    {
+        public XamarinFPieChartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
